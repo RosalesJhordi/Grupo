8c427b8c61e802cf9d32d8b4c027f324a3cd91bf
RosalesJhordi <1428060@senati.pe>
2023-08-17 12:26:46 -0500

    Creacion de variables y Primera visulizacion

--- OpMatematicas/src/main/java/com/mycompany/opmatematicas/OpMatematicas.java
+++ OpMatematicas/src/main/java/com/mycompany/opmatematicas/OpMatematicas.java
@@ -13,5 +13,14 @@ public class OpMatematicas {
     public static void main(String[] args) {
         // importamos la clase scanner
         Scanner a1 = new Scanner(System.in);
+        
+        //Creacion de Variables
+        
+        int n1,n2;
+        String op;
+        
+        // Priemra visualizacion
+        System.out.print("INGRESE SU OPCION \n 1: SUMA \n 2: RESTA \n 3: MULTIPLICACION \n 3: DIVISION \n Opcion: ");
+        op = a1.next();
     }
 }
