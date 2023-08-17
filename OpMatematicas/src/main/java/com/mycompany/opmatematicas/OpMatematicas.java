321f659b4bd82c37a2edc0e2397169f71efe32af
jost3 <1427593@senati.pe>
2023-08-17 12:38:19 -0500

    creacion de switch y case 1

--- OpMatematicas/src/main/java/com/mycompany/opmatematicas/OpMatematicas.java
+++ OpMatematicas/src/main/java/com/mycompany/opmatematicas/OpMatematicas.java
@@ -16,11 +16,25 @@ public class OpMatematicas {
         
         //Creacion de Variables
         
-        int n1,n2;
+        int n1,n2,suma,resta,mult,div;
         String op;
         
         // Priemra visualizacion
-        System.out.print("INGRESE SU OPCION \n 1: SUMA \n 2: RESTA \n 3: MULTIPLICACION \n 3: DIVISION \n Opcion: ");
+        System.out.print("INGRESE SU OPCION \n 1: SUMA \n 2: RESTA"
+                + " \n 3: MULTIPLICACION \n 3: DIVISION \n Opcion: ");
         op = a1.next();
+        //creacion del switch y case1-suma
+        switch(op){
+       
+            case "1":
+                System.out.print("\t suma \n ingresa el primer numero: ");
+                n1 = a1.nextInt();
+                System.out.print("ingresa el segundo numero: ");
+                n2 = a1.nextInt();
+                
+                suma = n1+n2;
+                
+                System.out.println("la suma es: "+suma+"\n");
+    }
     }
 }
